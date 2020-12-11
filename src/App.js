2508b7ee9befd27720effd6c9137a58039f6233a
mazhengrong25 <mazhengrong25@163.com>
2020-12-11 14:46:28 +0800

    bdjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+/*
+ * @Author: your name
+ * @Date: 2020-12-11 11:39:27
+ * @LastEditTime: 2020-12-11 14:38:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \positiond:\tests\react\react_test\src\App.js
+ */
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +23,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
